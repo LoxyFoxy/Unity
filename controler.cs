3a472a503e17487d69f82b86298ae75eca9cf3e6
LoxyFoxy <34635480+LoxyFoxy@users.noreply.github.com>
2018-05-14 09:56:29 +0900

    Add files via upload

---
+++ controler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controler : MonoBehaviour {
+	float h;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		h = Input.GetAxis("Horizontal");
+		Vector3 move = new Vector3 (h, 0, 0);
+		rb.AddForce (move * 40f);
+	}
+}
---
+++ spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+	public Rigidbody obj;
+	Transform tr;
+	Rigidbody clone;
+	void Start () {
+		tr = GetComponent<Transform> ();
+		InvokeRepeating ("enemy", 1f, 2f);
+	}
+	
+
+	void Update () {
+		
+	}
+	void enemy(){
+		clone=Instantiate (obj, new Vector3(Random.Range(-2.5f,3.1f),tr.position.y,tr.position.z), tr.rotation);
+		Vector3 force = new Vector3 (0,-100f,0);
+		clone.AddForce (force);
+	}
+}
